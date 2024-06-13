e60eecc776ce286b89d1ffa7f1a50d8b77f0a90c
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-13 19:42:35 +0530

    Updated and new version of previous project

---
+++ src/test/java/com/airbnb1/Airbnb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbnb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Airbnb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
