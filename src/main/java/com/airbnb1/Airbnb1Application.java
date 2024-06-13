4a32cf4fbc46bc69ad79c41a2a95ea0d5a92e50f
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-13 19:42:24 +0530

    Updated and new version of previous project

---
+++ src/main/java/com/airbnb1/Airbnb1Application.java
@@ -0,0 +1,13 @@
+package com.airbnb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Airbnb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Airbnb1Application.class, args);
+	}
+
+}
