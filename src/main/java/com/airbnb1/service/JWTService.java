3a111c0b780c5936562753738850d5041ecc7e06
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-14 09:52:22 +0530

    only Updated

---
+++ src/main/java/com/airbnb1/service/JWTService.java
@@ -0,0 +1,38 @@
+package com.airbnb1.service;
+
+import com.airbnb1.entity.PropertyUser;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+
+import java.util.Date;
+
+@Service
+public class JWTService {
+    @Value("${jwt.algorithm.key}")//what does value annotaion is does read the details from properties files and directly initialize the variable
+    private String algorithmKey;
+    @Value("${jwt.issuer}")
+    private  String issuer; //some client send the token then verify,hacker cannot create dummy token
+    @Value("${jwt.expiry.duration}")
+    private int expiryTime; //expiry time is calculated in miliseconds.
+
+    private Algorithm algorithm;
+    // i want to inject the value in it.hardcoding value is injected a create a problem
+
+    private final static String USER_NAME="username";
+    @PostConstruct //what is postConstruct annotaion does when you start appliaction it will start automatically
+    public void  postConstruct(){
+        algorithm = Algorithm.HMAC256(algorithmKey);
+
+    }
+    public String generateToken(PropertyUser propertyUser){
+        return JWT.create().
+                withClaim(USER_NAME,propertyUser.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+expiryTime))
+                .withIssuer(issuer)
+                .sign(algorithm);
+    }
+}
