3473fb577b364d9d91635ecd27b1345ab5405984
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-14 09:53:44 +0530

    only Updated

--- src/main/java/com/airbnb1/repository/PropertyUserRepository.java
+++ src/main/java/com/airbnb1/repository/PropertyUserRepository.java
@@ -3,8 +3,10 @@ package com.airbnb1.repository;
 import com.airbnb1.entity.PropertyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PropertyUserRepository extends JpaRepository<PropertyUser, Long> {
 
-
+    Optional<PropertyUser> findByUsername(String username);
 
 }
