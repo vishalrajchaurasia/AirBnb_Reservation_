da95c672b9ee9ccfd27d83b4932c939c998434d3
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-15 11:38:26 +0530

    only Updated

--- src/main/java/com/airbnb1/service/JWTService.java
+++ src/main/java/com/airbnb1/service/JWTService.java
@@ -3,6 +3,7 @@ package com.airbnb1.service;
 import com.airbnb1.entity.PropertyUser;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -34,5 +35,9 @@ public class JWTService {
                 .withExpiresAt(new Date(System.currentTimeMillis()+expiryTime))
                 .withIssuer(issuer)
                 .sign(algorithm);
+    }
+    public String getUserName(String token){
+        DecodedJWT decodedJWT =JWT.require(algorithm).withIssuer(issuer).build().verify(token);
+        return decodedJWT.getClaim(USER_NAME).asString();
     }
 }
