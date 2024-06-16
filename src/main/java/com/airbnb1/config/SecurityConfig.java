d7afc7690b70b62d8282acdb69e4fb4935c68e94
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-16 11:30:16 +0530

    only Updated

--- src/main/java/com/airbnb1/config/SecurityConfig.java
+++ src/main/java/com/airbnb1/config/SecurityConfig.java
@@ -21,11 +21,11 @@ public class SecurityConfig {//why used that because we open the Url
         http.csrf().disable().cors().disable();//this will work 3.0.0 version of spring security
         http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);//when we run the file config file will automatically run//it will run our custom filter method first
         //where is custom filter method is public class JWTRequestFilter extends OncePerRequestFilter
-        http.authorizeHttpRequests().
-        requestMatchers("/api/v1/users/addUser","/api/v1/users/login").permitAll()//this url every one can access//using java 8 remove request and replace with antMathcer
-                .requestMatchers("api/v1/countries/addCountry").hasRole("ADMIN")
-                .requestMatchers("api/v1/users/profile").hasAnyRole("ADMIN","USER")
-        .anyRequest().authenticated();//it is securing other URL
+        http.authorizeHttpRequests().anyRequest().permitAll();
+//        requestMatchers("/api/v1/users/addUser","/api/v1/users/login").permitAll()//this url every one can access//using java 8 remove request and replace with antMathcer
+//                .requestMatchers("api/v1/countries/addCountry").hasRole("ADMIN")
+//                .requestMatchers("api/v1/users/profile").hasAnyRole("ADMIN","USER")
+//        .anyRequest().authenticated();//it is securing other URL
         return http.build();//here http is object the build method all the information into that object and its return back SecurityFilterChain and then goes to a spring security
     }
 
