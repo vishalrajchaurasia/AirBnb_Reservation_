5f1ba726b48cbd14506ec0110d5485766a495caa
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-17 14:05:08 +0530

    only Updated

--- src/main/java/com/airbnb1/entity/Review.java
+++ src/main/java/com/airbnb1/entity/Review.java
@@ -1,4 +1,21 @@
 package com.airbnb1.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "review")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
