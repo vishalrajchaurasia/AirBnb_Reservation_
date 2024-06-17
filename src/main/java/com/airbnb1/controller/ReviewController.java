fe98960061a911bf77935e6e150412c34c9b99a2
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-17 14:53:34 +0530

    only Updated

--- src/main/java/com/airbnb1/controller/ReviewController.java
+++ src/main/java/com/airbnb1/controller/ReviewController.java
@@ -1,5 +1,7 @@
 package com.airbnb1.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
 
+    @PostMapping
+    public ResponseEntity<String> addReview(){
+
+    }
 }
