842dc4a898b49654cd5f2059acf51d9510b13cab
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-17 16:54:53 +0530

    only Updated

--- src/main/java/com/airbnb1/controller/ReviewController.java
+++ src/main/java/com/airbnb1/controller/ReviewController.java
@@ -33,6 +33,11 @@ public class ReviewController {
             @RequestBody Review review,//review is come from ReviewDto
             @AuthenticationPrincipal PropertyUser user //user is given the review for particular a particular property //it is automatically fetch the details user currently logged in
     ){//current user logged in will give the review i can find the current user used in @AuthenticationPrincipal
+        Review r= reviewRepository.findReviewByUserIdAndPropertyId(propertyId, user.getId());
+        if(r!=null){
+            return new ResponseEntity<>("You have already reviewed this property", HttpStatus.BAD_REQUEST);
+        }
+
         Optional<Property> opProperty = propertyRepository.findById(propertyId);
         Property property = opProperty.get();
         review.setProperty(property);
