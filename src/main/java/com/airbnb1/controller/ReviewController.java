0e492db0df9741c0e5f4c8c328f04141116ad949
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-17 14:13:19 +0530

    only Updated

---
+++ src/main/java/com/airbnb1/controller/ReviewController.java
@@ -0,0 +1,10 @@
+package com.airbnb1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+
+}
