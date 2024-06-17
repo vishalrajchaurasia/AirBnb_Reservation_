68d7536496b809d78d6e396ceb07f20e7ac1a23b
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-17 14:07:17 +0530

    only Updated

--- src/main/java/com/airbnb1/entity/Review.java
+++ src/main/java/com/airbnb1/entity/Review.java
@@ -10,6 +10,41 @@ public class Review {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "content", length = 1000)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    @ManyToOne
+    @JoinColumn(name = "property_user_id")
+    private PropertyUser propertyUser;
+
+    public PropertyUser getPropertyUser() {
+        return propertyUser;
+    }
+
+    public void setPropertyUser(PropertyUser propertyUser) {
+        this.propertyUser = propertyUser;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     public Long getId() {
         return id;
     }
