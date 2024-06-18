636c6473baf5887a8120df9e0ece2dc63ae53d3f
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-18 12:27:13 +0530

    only Updated

--- src/main/java/com/airbnb1/repository/ReviewRepository.java
+++ src/main/java/com/airbnb1/repository/ReviewRepository.java
@@ -1,11 +1,14 @@
 package com.airbnb1.repository;
 
+import com.airbnb1.entity.Property;
+import com.airbnb1.entity.PropertyUser;
 import com.airbnb1.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("select r from Review r where r.property.id = :propertyId and r.propertyUser.id = :userId")
-    Review findReviewByUserIdAndPropertyId(@Param("userId") long userId, @Param("propertyId") long propertyId);
+
+    @Query("select r from Review r where r.property = :property and r.propertyUser = :user")
+    Review findReviewByUser(@Param("property") Property property,@Param("user") PropertyUser user);
 }
