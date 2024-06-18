53d995dfeba0f95ece3468c5263c289ca50acf5f
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-18 13:03:20 +0530

    only Updated

--- src/main/java/com/airbnb1/repository/ReviewRepository.java
+++ src/main/java/com/airbnb1/repository/ReviewRepository.java
@@ -7,8 +7,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("select r from Review r where r.property = :property and r.propertyUser = :user")
     Review findReviewByUser(@Param("property") Property property,@Param("user") PropertyUser user);
+
+    //get all the review
+    List<Review> findByPropertyUser(PropertyUser user);
+
+
 }
