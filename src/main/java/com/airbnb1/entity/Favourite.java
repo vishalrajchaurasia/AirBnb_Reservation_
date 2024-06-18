98a6c1cc22ce58ddb4386f7a5ca6bd03451c954b
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-18 14:58:28 +0530

    only Updated

---
+++ src/main/java/com/airbnb1/entity/Favourite.java
@@ -0,0 +1,32 @@
+package com.airbnb1.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "favourite")
+public class Favourite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "is_favourite", nullable = false)
+    private Boolean isFavourite = false;
+
+    public Boolean getIsFavourite() {
+        return isFavourite;
+    }
+
+    public void setIsFavourite(Boolean isFavourite) {
+        this.isFavourite = isFavourite;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
