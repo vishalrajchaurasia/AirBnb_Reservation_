15e5e88bbec8bcda8f2bb19ea21e03bd1160cef7
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-18 13:01:12 +0530

    only Updated

--- src/main/java/com/airbnb1/repository/PropertyRepository.java
+++ src/main/java/com/airbnb1/repository/PropertyRepository.java
@@ -1,6 +1,7 @@
 package com.airbnb1.repository;
 
 import com.airbnb1.entity.Property;
+import com.airbnb1.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +14,5 @@ public interface PropertyRepository extends JpaRepository<Property, Long> {
     //here i use joins in my project
     @Query("select p from Property p JOIN Location l on p.location=l.id JOIN Country c on p.country=c.id where l.locationName=:locationName or c.countryName=:locationName")//what is p here is a property class and what consists of variables like id ,consits of property name,location..etc
     List<Property> findPropertyByLocation(@Param("locationName") String locationName);
+
 }
