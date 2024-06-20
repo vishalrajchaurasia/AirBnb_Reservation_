56247cf0162faac929c2772121125b43b2834ab4
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-20 14:54:58 +0530

    only Updated

--- src/main/java/com/airbnb1/config/AWSS3Config.java
+++ src/main/java/com/airbnb1/config/AWSS3Config.java
@@ -1,4 +1,20 @@
 package com.airbnb1.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AWSS3Config {
+    @Value("${accessKey}")
+    private String accessKey;
+    @Value("${secretKey}")
+    private String secretKey;
+    @Value("${region}")
+    private String region;
+    public AWSCredentials credentials(){
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return credentials;
+    }
 }
