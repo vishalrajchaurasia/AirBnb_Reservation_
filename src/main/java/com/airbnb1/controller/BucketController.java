743cf164911348af1e41ccb88f27fe063acad494
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-21 15:57:07 +0530

    in contoller layer add BucketController.java

--- src/main/java/com/airbnb1/controller/BucketController.java
+++ src/main/java/com/airbnb1/controller/BucketController.java
@@ -1,4 +1,23 @@
 package com.airbnb1.controller;
+import com.s3example1.service.BucketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+@RestController
+@RequestMapping("s3bucket")
+@CrossOrigin("*")//CroossOrigin is access API in angular
 public class BucketController {//this is not a BucketController its a imageBucketController
+
+    @Autowired
+    BucketService service;
+
+    @PostMapping(path = "/upload/file/{bucketName}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file,
+                                             @PathVariable String bucketName) {
+        return new ResponseEntity<>(service.uploadFile(file,bucketName), HttpStatus.OK);
+    }
 }
