3b8aedc0f2d1ccaf2393fa759a991f437bcc4b42
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-22 13:05:06 +0530

    .

---
+++ src/main/java/com/airbnb1/controller/ImageController.java
@@ -0,0 +1,49 @@
+package com.airbnb1.controller;
+
+import com.airbnb1.entity.Images;
+import com.airbnb1.entity.Property;
+import com.airbnb1.entity.PropertyUser;
+import com.airbnb1.repository.ImagesRepository;
+import com.airbnb1.repository.PropertyRepository;
+import com.airbnb1.service.BucketService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1/images")
+public class ImageController {
+    private ImagesRepository imagesRepository;
+    //now when you upload a images and you also have supply the property id no.
+    private PropertyRepository propertyRepository;
+    private BucketService bucketService;
+
+    public ImageController(ImagesRepository imagesRepository, PropertyRepository propertyRepository, BucketService bucketService) {
+        this.imagesRepository = imagesRepository;
+        this.propertyRepository = propertyRepository;
+        this.bucketService = bucketService;
+    }
+
+    @PostMapping(path = "/upload/file/{bucketName}/property/{propertyId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> uploadFile(@RequestParam MultipartFile file,
+                                             @PathVariable String bucketName,
+                                             @PathVariable long propertyId,
+                                             @AuthenticationPrincipal PropertyUser user
+
+    ) {
+        String imageUrl = bucketService.uploadFile(file, bucketName);//this will help me upload a images
+        Property property = propertyRepository.findById(propertyId).get();
+        Images img = new Images();
+        img.setImageUrl(imageUrl);
+        img.setProperty(property);
+        img.setPropertyUser(user);
+
+        Images savedImage = imagesRepository.save(img);
+
+
+        return new ResponseEntity<>(savedImage, HttpStatus.OK);
+    }
+}
