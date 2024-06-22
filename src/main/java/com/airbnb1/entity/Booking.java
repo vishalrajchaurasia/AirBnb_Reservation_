dc56145c24d150291041d733d61ba6805f62818d
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-22 16:13:54 +0530

    Add passenger entity

--- src/main/java/com/airbnb1/entity/Booking.java
+++ src/main/java/com/airbnb1/entity/Booking.java
@@ -10,6 +10,52 @@ public class Booking {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "total_price")
+    private Integer totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "property_user_id")
+    private PropertyUser propertyUser;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    @Column(name = "guest_name", nullable = false)
+    private String guestName;
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public PropertyUser getPropertyUser() {
+        return propertyUser;
+    }
+
+    public void setPropertyUser(PropertyUser propertyUser) {
+        this.propertyUser = propertyUser;
+    }
+
+    public Integer getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Integer totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
     public Long getId() {
         return id;
     }
