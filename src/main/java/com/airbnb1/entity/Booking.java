d83344e850a56a29c90fe46cacf27ec2159314f2
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-22 16:07:06 +0530

    Add passenger entity

---
+++ src/main/java/com/airbnb1/entity/Booking.java
@@ -0,0 +1,21 @@
+package com.airbnb1.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "booking")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
