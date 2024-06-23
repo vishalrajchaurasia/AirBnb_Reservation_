2a8c2c03ae90172f1daea2d761136057790ef223
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-23 22:36:53 +0530

    .

--- src/main/java/com/airbnb1/service/PDFService.java
+++ src/main/java/com/airbnb1/service/PDFService.java
@@ -1,4 +1,30 @@
 package com.airbnb1.service;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.FileOutputStream;
+
+@Service
 public class PDFService {
+    private static final String PDF_DIRECTORY = "/path/to/your/pdf/directory";
+    public void generatePDF(){
+        try {
+            //Generate unique filename for the PDF
+            Document document = new Document();//firstly create a document object
+            PdfWriter.getInstance(document, new FileOutputStream("C://feb//iTextHelloWorld.pdf"));
+
+            document.open();//opening of the document
+            Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);//and here setting of the font and color. what you font want in your PDF
+            Chunk chunk = new Chunk("Hello World", font);
+
+            document.add(chunk);
+            document.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            //return null;
+        }
+    }
 }
