232be2b1f47f0c620146f138f5e769a9c68c0d3b
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-23 18:11:50 +0530

    .

--- src/main/java/com/airbnb1/controller/BookingController.java
+++ src/main/java/com/airbnb1/controller/BookingController.java
@@ -1,31 +1,43 @@
 package com.airbnb1.controller;
 
 import com.airbnb1.entity.Booking;
+import com.airbnb1.entity.Property;
 import com.airbnb1.entity.PropertyUser;
 import com.airbnb1.repository.BookingRepository;
+import com.airbnb1.repository.PropertyRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/booking")
 public class BookingController {
     private BookingRepository bookingRepository;
+    private PropertyRepository propertyRepository;
 
-    public BookingController(BookingRepository bookingRepository) {
+    public BookingController(BookingRepository bookingRepository, PropertyRepository propertyRepository) {
         this.bookingRepository = bookingRepository;
+        this.propertyRepository = propertyRepository;
     }
-    @PostMapping("/createBooking")
+    @PostMapping("/createBooking/{propertyId}")
     public ResponseEntity<Booking> createBooking(
             @RequestBody Booking booking,//in this case booking details itself be capture the property details
-            @AuthenticationPrincipal PropertyUser user
+            @AuthenticationPrincipal PropertyUser user,
+            @PathVariable long propertyId
 
             ){
         booking.setPropertyUser(user);
+//        //Property property = booking.getProperty();//what will do line it go to the booking class it will help me the get the property object address supply here @RequestBody Booking booking
+//       // Long propertyId = property.getId();
+//       // Property completePropertyInfo = propertyRepository.findById(propertyId).get();
+//       // Booking createdBooking = bookingRepository.save(booking);
+        Property property = propertyRepository.findById(propertyId).get();
+        int propertyPrice = property.getNightlyPrice();
+        int totalNights = booking.getTotalNights();
+        int totalPrice = propertyPrice * totalNights;
+        booking.setProperty(property);
+        booking.setTotalPrice(totalPrice);
         Booking createdBooking = bookingRepository.save(booking);
         return new ResponseEntity<>(createdBooking, HttpStatus.CREATED);
     }
