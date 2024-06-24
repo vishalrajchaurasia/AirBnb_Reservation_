3868438a294e6e82ba5f37e5138756947476919e
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-24 14:32:45 +0530

    .

--- src/main/java/com/airbnb1/service/PDFService.java
+++ src/main/java/com/airbnb1/service/PDFService.java
@@ -1,5 +1,6 @@
 package com.airbnb1.service;
 
+import com.airbnb1.dto.BookingDto;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,7 @@ import java.io.FileOutputStream;
 @Service
 public class PDFService {
     private static final String PDF_DIRECTORY = "/path/to/your/pdf/directory";
-    public void generatePDF(String fileName){//this file name iTextHelloWorld.pdf is come from here
+    public void generatePDF(String fileName, BookingDto dto){//this file name iTextHelloWorld.pdf is come from here
         try {
             //Generate unique filename for the PDF
             Document document = new Document();//firstly create a document object
@@ -17,9 +18,16 @@ public class PDFService {
 //pdfWriter class  comes from itext pdf //why output because it deal with pdf file xl file etc...
             document.open();//opening of the document
             Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);//and here setting of the font and color. what you font want in your PDF
-            Chunk chunk = new Chunk("Hello World", font);
+            Chunk bookingConfirmation = new Chunk("Booking Confirmation", font);
+            Chunk guestName = new Chunk("Guest Name: "+dto.getGuestName(), font);
+            Chunk price = new Chunk("Price Per Night"+dto.getPrice(), font);
+            Chunk totalPrice = new Chunk("Total Price"+dto.getTotalPrice(), font);
 
-            document.add(chunk);
+            document.add(bookingConfirmation);
+            document.add(guestName);
+            document.add(price);
+            document.add(totalPrice);
+
             document.close();
         }
         catch (Exception e) {
