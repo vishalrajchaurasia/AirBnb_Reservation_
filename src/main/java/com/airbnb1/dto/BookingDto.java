8dc21fa0d226811b3dd6bf6a61285c6a12750995
vishalrajchaurasia <vchaurasia7@gmail.com>
2024-06-24 14:32:25 +0530

    .

--- src/main/java/com/airbnb1/dto/BookingDto.java
+++ src/main/java/com/airbnb1/dto/BookingDto.java
@@ -1,4 +1,40 @@
 package com.airbnb1.dto;
 
 public class BookingDto {
+    private long bookingId;
+    private String guestName;
+    private int price;
+    private int totalPrice;
+
+    public long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
